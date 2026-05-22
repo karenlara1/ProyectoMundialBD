c60e1b990c3bc6d76150e63eaa464213a0b26e93
karenlara1 <karena.larab@uqvirtual.edu.co>
2026-05-22 14:47:23 -0500

    Unión ramas master, Allison y David

--- src/main/java/co/edu/uniquindio/conexion/ConexionOracle.java
+++ src/main/java/co/edu/uniquindio/conexion/ConexionOracle.java
@@ -8,7 +8,7 @@ public class ConexionOracle {
 
     private static final String URL ="jdbc:oracle:thin:@localhost:1521/xe";
     private static final String USUARIO = "SYSTEM"; //Usuario con el que se crea la BD
-    private static final String CONTRASENIA = ""; //Contraseña utilizada para crear BD
+    private static final String CONTRASENIA = "bella"; //Contraseña utilizada para crear BD
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, CONTRASENIA);
