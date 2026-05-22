b7cf3342c08a527af7f445b40602ad1a48e779e2
karenlara1 <karena.larab@uqvirtual.edu.co>
2026-05-21 22:10:41 -0500

    Creación clase Sesion

---
+++ src/main/java/co/edu/uniquindio/util/Sesion.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.util;
+
+import co.edu.uniquindio.modelo.Usuario;
+
+public class Sesion {
+
+    public static Usuario usuarioActual;
+    public static int idBitacoraActual;
+}
